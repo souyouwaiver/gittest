3d791463c3da34e3e15ab329863e6834406cbabf
lzf <2155703714@qq.com>
2021-07-22 15:36:00 +0800

    first commit

---
+++ src/main/java/com/dragon/service/GitTest.java
@@ -0,0 +1,9 @@
+package com.dragon.service;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git2");
+    }
+
+}
